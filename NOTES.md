39b45133d5e317eec7582b9f56f72f9ea1fe1a2b
Lichfolky <lichfolky@gmail.com>
2023-05-05 10:14:24 +0200

    Add Straming video

--- NOTES.md
+++ NOTES.md
@@ -15,5 +15,7 @@ Material lite
 https://getmdl.io/
 
 
-
+Aggiungi questa classe per avere il bk corretto:
 mat-app-background
+
+https://www.figma.com/file/xEtEkig3E6YOeTQ5hjAYlj/ng2-class-project?node-id=0%3A1&t=N5OvPhqcKRW027Ob-1
--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,14 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { YouTubePlayer } from '@angular/youtube-player';
+let apiLoaded = false;
 
 @Component({
+  // template: '<youtube-player videoId="PRQCAL_RMVo"></youtube-player>',
   selector: 'app-video',
   templateUrl: './video.component.html',
   styleUrls: ['./video.component.css']
 })
-export class VideoComponent {
-  @Input() title: string = "Animals";
+export class VideoComponent implements OnInit {
+  @Input() videoId: string = "px7dr7ofHv0";
+  playerVars = { 'autoplay': 1, 'controls': 0 };
 
   url: string = "https://www.youtube.com/embed/px7dr7ofHv0";
+  player: YouTubePlayer | undefined;
 
+  ngOnInit() {
+    if (!apiLoaded) {
+      // This code loads the IFrame Player API code asynchronously, according to the instructions at
+      // https://developers.google.com/youtube/iframe_api_reference#Getting_Started
+      const tag = document.createElement('script');
+      tag.src = 'https://www.youtube.com/iframe_api';
+      document.body.appendChild(tag);
+      apiLoaded = true;
+    }
+  }
 
+  onReady(event: YT.PlayerEvent) {
+    let player = event.target;
+    player.mute();
+  }
 }
